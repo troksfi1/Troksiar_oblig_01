c8fd7dbaef07a90a9184e1f6bf7fa9fec3d27326
IvanMunozReina <72027802+IvanMunozReina@users.noreply.github.com>
2021-03-08 08:39:14 +0100

    FirstTest Complete

--- app/src/androidTest/java/no/hvl/dat153/troksiar_oblig_01/FirstTest.java
+++ app/src/androidTest/java/no/hvl/dat153/troksiar_oblig_01/FirstTest.java
@@ -42,10 +42,14 @@ import androidx.room.Room;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.annotation.UiThreadTest;
 import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -76,7 +80,23 @@ import no.hvl.dat153.troksiar_oblig_01.data.ItemRoomDatabase;
 import no.hvl.dat153.troksiar_oblig_01.data.ItemViewModel;
 
 import android.app.Application;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
 import java.util.List;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 
 //@LargeTest
@@ -129,7 +149,59 @@ public class FirstTest{
 
     @Test
     public void firstTest() {
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.quiz_photo_name),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText5.perform(replaceText("Julia"), closeSoftKeyboard());
 
+        ViewInteraction appCompatEditText1 = onView(
+                allOf(withId(R.id.quiz_photo_name), withText("Julia"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText1.perform(pressImeActionButton());
+
+        ViewInteraction appCompatButton6 = onView(
+                allOf(withId(R.id.button_check), withText("Check"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatButton6.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.score), withText("Score: 1 of 1"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("Score: 1 of 1")));
+    }
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 
 }
